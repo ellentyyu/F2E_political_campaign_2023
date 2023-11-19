dff10fee86092618a270b5977017f486158c84c0
ellen <ellentyyu@gmail.com>
2023-11-19 15:38:40 +0800

    feat: 調整header/banner樣式

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,15 +2,17 @@ import imgCandidate from '../assets/img-banner-miao.png';
 
 const Banner = () => {
   return (
-    <div className="banner px-4 py-32">
+    <div className="banner px-4">
         <div className="container mx-auto">
             <div className='flex flex-col justify-center items-center'>
-                <h2 className="mb-4 section-title-lg">台灣的明天<br className="lg:hidden" />喵先鋪路</h2>
-                <div className="flex flex-col md:flex-row justify-center items-center mb-4">
-                    <h3 className="section-subtitle-lg mr-0 mb-2 md:mr-4 md:mb-0">2024 立委參選人</h3>
-                    <div className="flex items-center px-4 py-3 shadow-primary rounded-2xl">
-                        <i className="shrink-0 w-10 h-10 mr-4 icon-standard bg-number"></i>
-                        <h1 className="text-[28px] leading-[150%] lg:text-[40px] font-bold text-coral-700">喵立翰 Miao Li-Han</h1>
+                <div className='pt-8 pb-4'>
+                    <h2 className="mb-4 section-title-lg">台灣的明天<br className="lg:hidden" />喵先鋪路</h2>
+                    <div className="flex flex-col md:flex-row justify-center items-center">
+                        <h3 className="section-subtitle-lg mr-0 mb-2 md:mr-4 md:mb-0">2024 立委參選人</h3>
+                        <div className="flex items-center px-4 py-3 shadow-primary rounded-2xl">
+                            <i className="shrink-0 w-10 h-10 mr-4 icon-standard bg-number"></i>
+                            <h1 className="text-[28px] leading-[150%] lg:text-[40px] font-bold text-coral-700">喵立翰 Miao Li-Han</h1>
+                        </div>
                     </div>
                 </div>
                 <img className='banner__img img-fit' src={imgCandidate} alt="candidate portrait" />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,45 @@
-import logo from '../assets/icon-logo.svg';
-
 const Header = () => {
     return (
-        // bg-coral-50
-        <header className="fixed top-0 left-0 w-full px-6 py-2 bg-coral-700">
-            <div className="container mx-auto">
-                <nav className="flex justify-between items-center px-6 py-3 bg-white rounded-2xl">
-                    <a className="flex items-center" href="#">
-                        {/* <img
-                            className="w-[34px] h-[28px] md:w-[52px] md:h-[43px] mr-2 icon-standard"
-                            src={logo}
-                            alt="campaign logo"
-                        /> */}
-                        <i className="w-[34px] h-[28px] md:w-[52px] md:h-[43px] mr-2 icon-standard bg-logo"></i>
-                        <h1 className="text-xl md:text-2xl font-bold text-coral-700">
-                            喵立翰 Miao Li-Han
-                        </h1>
-                    </a>
-                    <ul className='hidden lg:flex items-center lg:gap-1 xl:gap-4'>
-                        <li>
-                            <a className='p-2 text-lg text-slate-700' href="#">候選人主張</a>
-                        </li>
-                        <li>
-                            <a className='p-2 text-lg text-slate-700' href="#">最新活動</a>
-                        </li>
-                        <li>
-                            <a className='p-2 text-lg text-slate-700' href="#">政策議題</a>
-                        </li>
-                        <li>
-                            <a className='p-2 text-lg text-slate-700' href="#">小額捐款</a>
-                        </li>
-                        <li>
-                            <a className='p-2 text-lg text-slate-700' href="#">民眾服務信箱</a>
-                        </li>
-                    </ul>
-                    <div className='hidden md:flex item-center gap-4'>
-                        <a href="#">
-                            <i className="w-8 h-8 icon-standard fb"></i>
+        <header className="w-full h-[72px] md:h-[100px] bg-coral-50">
+            <div className='fixed top-0 left-0 right-0 md:top-4 md:left-6 md:right-6'>
+                <div className="container mx-auto">
+                    <nav className="flex justify-between items-center px-6 py-3 bg-white md:rounded-2xl">
+                        <a className="flex items-center" href="#">
+                            <i className="w-[34px] h-[28px] md:w-[52px] md:h-[43px] mr-2 icon-standard bg-logo"></i>
+                            <h1 className="text-xl md:text-2xl font-bold text-coral-700">
+                                喵立翰 Miao Li-Han
+                            </h1>
                         </a>
-                        <a href="#">
-                            <i className="w-8 h-8 icon-standard ig"></i>
-                        </a>
-                        <a href="#">
-                            <i className="w-8 h-8 icon-standard yt"></i>
-                        </a>
-                    </div>
-                </nav>
+                        <ul className='hidden lg:flex items-center lg:gap-1 xl:gap-4'>
+                            <li>
+                                <a className='p-2 text-lg text-slate-700' href="#">候選人主張</a>
+                            </li>
+                            <li>
+                                <a className='p-2 text-lg text-slate-700' href="#">最新活動</a>
+                            </li>
+                            <li>
+                                <a className='p-2 text-lg text-slate-700' href="#">政策議題</a>
+                            </li>
+                            <li>
+                                <a className='p-2 text-lg text-slate-700' href="#">小額捐款</a>
+                            </li>
+                            <li>
+                                <a className='p-2 text-lg text-slate-700' href="#">民眾服務信箱</a>
+                            </li>
+                        </ul>
+                        <div className='hidden md:flex item-center gap-4'>
+                            <a href="#">
+                                <i className="w-8 h-8 icon-standard fb"></i>
+                            </a>
+                            <a href="#">
+                                <i className="w-8 h-8 icon-standard ig"></i>
+                            </a>
+                            <a href="#">
+                                <i className="w-8 h-8 icon-standard yt"></i>
+                            </a>
+                        </div>
+                    </nav>
+                </div>
             </div>
         </header>
     );
